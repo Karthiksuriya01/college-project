af3c11c46c1412b39eb34531692411a97d9a3086
karthiksuriya01 <karthiksuriy01@gmail.com>
2025-01-19 01:45:51 +0530

    updated

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,10 @@
 import { SignedOut, SignInButton } from "@clerk/clerk-react";
+import { Input } from "./ui/input";
 
 const Header = () => {
   return (
-    <header className="fixed top-0 left-0 right-0 flex justify-end p-5 shadow z-50">
+    <header>
+      <Input/>
        <SignedOut>
           <SignInButton />
         </SignedOut>
--- src/layout/app-layout.tsx
+++ src/layout/app-layout.tsx
@@ -4,13 +4,18 @@ import { Outlet } from 'react-router-dom';
 
 const Applayout = () => {
   return (
-    <div>
-      <SideBar />
-      <Header/>
-      <div className='flex-1 p-4 sm:ml-64'>
+    <>
+     <SideBar />
+    <div className='flex flex-col my-10'>
+      <div className='fixed top-0 left-0 right-0 z-100 h-16 bg-gray-950'>
+        <Header/>
+
+      </div>
+      <div className='sm:ml-64 w-full h-screen p-4'>
         <Outlet />
       </div>
-    </div>
+     </div>
+    </>
   );
 }
 
